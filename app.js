dcd6fc7ff2178c608c5050b433102bedd50a16fc
CoralineBouyer <coralinebbouyer@gmail.com>
2022-03-22 17:43:21 +0100

    App fonctionnelle, manque le style

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const app = {
 
-    container: document.querySelector('#container'),
+    body: document.querySelector('body'),
+    button: document.querySelector('#button'),
+    time: document.querySelector('#time'),
+    players: document.querySelector('#players'),
 
     init: function () {
-        app.displayGame()
+        app.button.addEventListener('click', (event) => {
+            event.preventDefault();
+            if (document.querySelector('.container')) {
+                document.querySelector('.container').remove()
+            }            
+            app.chosenGame()
+        })
     },
 
     findData: async function () {
@@ -12,23 +21,60 @@ const app = {
         return games
     },
 
-    displayGame: async function () {
-        const games = await app.findData();
-        const index = app.randomIndex(0, games.length);
-        const chosenGame = document.createElement('div');
-        chosenGame.classList.add('chosenGame');
-        app.container.appendChild(chosenGame)
-        chosenGame.textContent = games[index].nom;
-
-        console.log(index)
-        console.log(games[index].nom)
-        // games.forEach(element => {
-        //     console.log(element.nom)
-        // });
-    },
-
     randomIndex: function (a, b) {
         return Math.floor(Math.random() * b);
-    }
+    },
+
+    // randomGame attend un array en paramètre
+    randomGame: async function (games) {
+        const container = document.createElement('div');
+        container.classList.add('container');
+        app.body.appendChild(container);        
+        // Je tire au sort l'index
+        const index = app.randomIndex(0, games.length);
+        // Je fais apparaître le nom dans une div
+        const chosenGame = document.createElement('div');
+        chosenGame.classList.add('chosenGame');
+        container.appendChild(chosenGame)
+        chosenGame.textContent = games[index].nom;
+        // Je fais apparaître l'image dans une balise img
+        const chosenPic = document.createElement('img');
+        chosenPic.classList.add('chosenPic');
+        chosenPic.setAttribute('src', games[index].img)
+        container.appendChild(chosenPic);
+    },
+
+    chosenGame: async function () {
+        const games = await app.findData();
+        // Si les deux sont cochés, je fais un tirage random sur les jeux qui se jouent à deux et font moins d'une heure etc
+        if (app.players.checked && app.time.checked) {
+            console.log("les deux cochés");
+            const gamesList = games.filter((game) => {
+                return (game.court && game.deux_joueurs);
+            });
+            console.log(gamesList);
+            app.randomGame(gamesList)
+            // Les jeux courts seulement, pour tous les joueurs
+        } else if (app.time.checked) {
+            console.log("Time coché");
+            const gamesList = games.filter((game) => {
+                return (game.court);
+            });            
+            console.log(gamesList);
+            app.randomGame(gamesList)
+            // Les jeux qui se jouent à deux, de toutes longueurs
+        } else if (app.players.checked) {
+            console.log("Joueurs cochés");
+            const gamesList = games.filter((game) => {
+                return (game.deux_joueurs);
+            });
+            console.log(gamesList);
+            app.randomGame(gamesList)
+            // Si rien n'est coché, je fais le tirage sur tous les jeux
+        } else { 
+            console.log("Rien n'est coché")
+            app.randomGame(games)
+        }
+    },
 }
 app.init()
